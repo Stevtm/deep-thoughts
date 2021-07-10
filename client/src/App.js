0ce8049c20ae420b3b296f5753dae67a326408ab
Stevtm <steventmaddison@outlook.com>
2021-07-10 00:31:24 -0400

    send token with all requests

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,10 @@ import {
 	createHttpLink,
 } from "@apollo/client";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NoMatch from "./pages/NoMatch";
@@ -21,8 +21,16 @@ const httpLink = createHttpLink({
 	uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem("id_token");
+	return {
+		...headers,
+		authorization: token ? `Bearer ${token}` : "",
+	};
+});
+
 const client = new ApolloClient({
-	link: httpLink,
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache(),
 });
 
